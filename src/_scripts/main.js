537005ff43d62b657d9d5f57f8b378b11deebd15
CarolEnache <enache.carol@gmail.com>
2017-01-11 12:43:59 +0000

    added max-width and css-position to the practiced properties menu

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -14,12 +14,21 @@ $(function() {
 function removeElement() {
   document.getElementById("imgbox1").style.display="none";
 }
-
+removeElement()
 function changeVisibility() {
   document.getElementById("imgbox2").style.visibility="hidden";
 }
+changeVisibility()
 
 function resetElement() {
   document.getElementById("imgbox1").style.display="block";
   document.getElementById("imgbox2").style.visibility="visible";
 }
+resetElement()
+
+function doSum(a, b) {
+  var sum = a + b;
+  return sum;
+}
+doSum(2, 5);
+doSum(6, 71);
